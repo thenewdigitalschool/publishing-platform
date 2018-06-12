6cc7e966d8eaa0b862731e1b3b80a13fb0e0159f
João Araújo <juauzaraujo@gmail.com>
2018-06-12 17:10:37 +0100

    Components folder added and initial files

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import Image from '../Components/Image';
 import Input from '../Components/Input';
+import LabelTitle from "../Components/Label";
 
 export default () => 
   <div>
     <Image src="http://via.placeholder.com/400x450" alt="Alternate Text" />
     <Input label="Input Label" type="text"/>
+    <LabelTitle labelTitle="yoo"/>
   </div>
