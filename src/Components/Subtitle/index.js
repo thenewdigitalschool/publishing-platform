70cf92230a9314bfaa1950e21c8daaaa80a36e94
João Justo <jpjustonunes@gmail.com>
2018-06-12 16:23:03 +0100

    new branch with text and subtitle

---
+++ src/Components/Subtitle/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class Subtitle extends Component {
+  render() {
+    return(
+      <h2 className="Subtitle">{this.props.subtitle}</h2>
+    );
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,18 @@
+<<<<<<< HEAD
 import React from "react"
 import Text from "../Components/Text";
 
 export default () => <div><Text mainText='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus faucibus erat vel suscipit. Pellentesque molestie sapien eu dolor bibendum, blandit blandit enim gravida. Phasellus et eros diam. Aenean elementum orci sem, id malesuada massa feugiat vitae. Phasellus nunc diam, suscipit eleifend diam id, gravida ultrices leo. Donec posuere felis orci, vel hendrerit ligula vestibulum ac. Integer blandit at augue eget congue.'/></div>
+||||||| (empty tree)
+=======
+import React from "react"
+import Subtitle from "../Components/Subtitle";
+
+<<<<<<< Updated upstream
+export default () => <div>Hello world!</div>
+>>>>>>> 61a580d... Initial commit
+||||||| merged common ancestors
+export default () => <div>Hello world!</div>
+=======
+export default () => <div><Subtitle subtitle='This is a subtitle'/></div>
+>>>>>>> Stashed changes
