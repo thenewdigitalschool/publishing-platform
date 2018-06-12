d4321d2e90367d5f7e65ca3c370d99bd7af085eb
Marissa <marissa.biesecker@gmail.com>
2018-06-12 17:25:46 +0100

    Add text component

---
+++ src/Components/Text/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class Text extends Component {
+  render(){
+    return(
+      <p className="mainText"></p>
+    );
+  }
+
+}
