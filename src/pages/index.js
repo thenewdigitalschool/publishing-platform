e17732cc0d4010af479e1139bb828664be6050c3
Marissa <marissa.biesecker@gmail.com>
2018-06-13 11:09:42 +0100

    Add prop; SUIT CSS naming, font family updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 import Text from "../Components/Text";
 
-export default () => <div><Text/></div>
+export default () => <div><Text mainText='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus faucibus erat vel suscipit. Pellentesque molestie sapien eu dolor bibendum, blandit blandit enim gravida. Phasellus et eros diam. Aenean elementum orci sem, id malesuada massa feugiat vitae. Phasellus nunc diam, suscipit eleifend diam id, gravida ultrices leo. Donec posuere felis orci, vel hendrerit ligula vestibulum ac. Integer blandit at augue eget congue.'/></div>
