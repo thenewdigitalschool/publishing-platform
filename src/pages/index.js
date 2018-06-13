1382a9e2e3b47d58417d8db05114f38f833620bc
Huzaifa <huzaifa.dpi@gmail.com>
2018-06-13 10:32:07 +0100

    Apply Link wwith new branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import React from "react";
 import Image from '../Components/Image';
 import Input from '../Components/Input';
 import LabelTitle from "../Components/Label";
+import ApplyLink from "../Components/Link"
 
-export default () => 
+export default () =>
   <div>
     <Image src="http://via.placeholder.com/400x450" alt="Alternate Text" />
-    <Input label="Input Label" type="text"/>
-    <LabelTitle labelTitle="yoo"/>
+    <Input label="Input Label" type="text" />
+    <LabelTitle labelTitle="yoo" />
+    <ApplyLink LinkText="Apply Now" LinkUrl="#" />
   </div>
