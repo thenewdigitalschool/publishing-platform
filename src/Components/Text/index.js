eda194f68f276cac1c0f89ac8f78e1d6c26d5b6d
Marissa <marissa.biesecker@gmail.com>
2018-06-13 11:18:38 +0100

    Add space between () {}

--- src/Components/Text/index.js
+++ src/Components/Text/index.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class Text extends Component {
-  render(){
+  render() {
     return(
       <p className="MainText">{this.props.mainText}</p>
     );
   }
-
 }
