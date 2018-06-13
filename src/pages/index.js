5b1363615e3540e3c60ba6c6163600e4811931c5
Huzaifa <huzaifa.dpi@gmail.com>
2018-06-13 17:11:42 +0100

    package jason & apply link fix
    package json has a unnessary comma after dependencies and it was causing problem while yarn develop. also I fixed apply link copy that was missing during merge

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,20 +7,20 @@ import LabelTitle from '../Components/Label';
 import ApplyLink from '../Components/Link';
 import Quote from '../Components/Quote';
 import Title from '../Components/Title';
-import Avatar from '../components/avatar'
+import Avatar from '../Components/avatar'
 
 export default () => (
   <div>
     <Image src="http://via.placeholder.com/400x450" alt="Alternate Text" />
     <Input label="Input Label" type="text" />
     <LabelTitle labelTitle="yoo" />
-    <ApplyLink LinkText="Apply Now" LinkUrl="#" />
+    <ApplyLink LinkUrl="#"> Apply Now </ApplyLink>
     <Quote>
       "In the school there is not a typical day, like, you always have a
       different activies, but the good thing is that you can decide which
       activities you want to be involved with."
     </Quote>
     <Title propTitle="Shit, It's Working" />
-    < Avatar src="https://unsplash.it/1200/800/?random"/>
-  </div>
+    < Avatar src="https://unsplash.it/1200/800/?random" />
+  </div >
 );
