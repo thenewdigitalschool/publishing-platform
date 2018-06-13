c5324fcb016565905029f0ffff23252770002f0a
Marissa <marissa.biesecker@gmail.com>
2018-06-13 12:10:53 +0100

    New changes

---
+++ src/Components/Text/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class Text extends Component {
+  render() {
+    return(
+      <p className="MainText">{this.props.mainText}</p>
+    );
+  }
+}
