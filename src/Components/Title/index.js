273ec7281ad3696ae0798ff061c26e4dfb948cd6
Marissa <marissa.biesecker@gmail.com>
2018-06-14 15:46:17 +0100

    Add text and subtitle components

--- src/Components/Title/index.js
+++ src/Components/Title/index.js
@@ -1,10 +1,8 @@
 import React, {Component} from "react"
 import "./index.css"
 
-export default class Title extends React.Component {
-    render(){
-        
-        return <h1 className="Title">{this.props.children}</h1>
-    }
-} 
-
+export default class Title extends Component {
+  render() {
+    return <h1 className="Title">{this.props.children}</h1>
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React from "react"
 
+import Text from "../Components/Text";
+import Subtitle from "../Components/Subtitle";
 import Image from '../Components/Image';
 import Input from '../Components/Input';
 import LabelTitle from '../Components/Label';
@@ -31,6 +33,7 @@ export default () => (
     <TndsLogo>
       why we do what we do
     </TndsLogo>
-
+    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus faucibus erat vel suscipit. Pellentesque molestie sapien eu dolor bibendum, blandit blandit enim gravida. Phasellus et eros diam. Aenean elementum orci sem, id malesuada massa feugiat vitae. Phasellus nunc diam, suscipit eleifend diam id, gravida ultrices leo. Donec posuere felis orci, vel hendrerit ligula vestibulum ac. Integer blandit at augue eget congue.</Text>
+    <Subtitle>This is a subtitle</Subtitle>
   </div>
 );
