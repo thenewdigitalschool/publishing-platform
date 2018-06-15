3f5b3e2483558c78530539faf13a90e5a78387bf
Huzaifa <huzaifa.dpi@gmail.com>
2018-06-15 11:28:59 +0100

    HOTFIX: fixed linter offenses

--- src/Components/Link/index.js
+++ src/Components/Link/index.js
@@ -3,17 +3,17 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class ApplyLink extends Component {
-    render() {
-        return (
-            <a className={this.props.classname} href={this.props.LinkUrl}>
-                {' '}
-                <span className="ApplyLink-text">{this.props.children}</span>{' '}
-            </a>
-        );
-    }
+  render() {
+    return (
+      <a className={this.props.classname} href={this.props.LinkUrl}>
+        <span className="ApplyLink-text">{this.props.children}</span>
+      </a>
+    );
+  }
 }
 
 ApplyLink.propTypes = {
-    LinkUrl: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
+  classname: PropTypes.string.isRequired,
+  LinkUrl: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
 };
