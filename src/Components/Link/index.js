56ebb58e02cc1aa2d43edd8fc2254aef2b26fca2
sarthak <sarthakveggalam@gmail.com>
2018-06-15 14:59:29 +0100

    removed comments from components

--- src/Components/Link/index.js
+++ src/Components/Link/index.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import './index.css'
 
 export default class ApplyLink extends Component {
-<<<<<<< Updated upstream
-    render() {
-        return (
-            <a className="ApplyLink" href={this.props.LinkUrl} > <span className="ApplyLink-text">{this.props.children}</span> </a>
-        )
-    }
-} 
-=======
   render() {
     return (
       <a className={this.props.classname} href={this.props.LinkUrl}>
@@ -24,4 +16,3 @@ ApplyLink.propTypes = {
   LinkUrl: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
->>>>>>> Stashed changes
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -4,45 +4,6 @@ import './index.css';
 import Link from '../Navigation/link';
 import TndsLogo from '../Logo';
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    export default class Navbar extends React.Component{
-        render(){
-            return (
-            <div className="nav-wrapper">
-                <div className="logo">
-                    <img src="./tnds_logo_white.svg" alt=""/>
-=======
-import Link from "../Navigation/link"
-import TndsLogo from "../Logo"
-export default class Navbar extends React.Component {
-    render(){
-        
-        return (
-            <div className="Navbar">
-                <div className="Navbar-Logo">
-                    <TndsLogo />
-                </div>
-                <div className="Navbar-Links">
-                    <Link  linkurl="" linklabel="About Us"/>
-                    <Link  linkurl="" linklabel="Full Time Program"/>
-                    <Link  linkurl="" linklabel="Living in Porto"/>
-                    <Link  linkurl="" linklabel="Apply Now"/>
->>>>>>> Stashed changes
-                </div>
-                <div className="links">
-                    <ul className="link-container">
-                        <a href=""><li className="link-item">ABOUT US</li></a>
-                        <a href=""><li className="link-item">FULL-TIME PROGRAM</li></a>
-                        <a href=""><li className="link-item">LIVING IN PORTO</li></a>
-                        <a href=""><button className="apply-button">Apply Now</button></a>
-                    </ul>
-                </div>  
-            </div>
-            )
-        }
-    }
-=======
 export default class Navbar extends React.Component {
   render() {
     return (
@@ -61,4 +22,4 @@ export default class Navbar extends React.Component {
   }
 }
 
->>>>>>> Stashed changes
+
