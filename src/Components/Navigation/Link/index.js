d0e40107fc7008564c5fbf820885d15a7cb9f6f1
sarthak <sarthakveggalam@gmail.com>
2018-07-03 11:31:04 +0100

    changes to navigation link component

--- src/Components/Navigation/Link/index.js
+++ src/Components/Navigation/Link/index.js
@@ -7,11 +7,11 @@ export default class Link extends Component {
   render() {
     return (
       <div>
-        <a className="Link" href={ this.props.linkurl }>{ this.props.linklabel } </a>
+        <a className="Link" href={this.props.linkurl}>{this.props.linklabel} </a>
       </div>
-      );
-    }
+    );
   }
+}
 
 
 Link.propTypes = {
