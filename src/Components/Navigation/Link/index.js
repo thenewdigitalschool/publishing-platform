80bdc0e96e71d2897d9555552a4576020e38beb7
fmmricardo <ricardofilipe5@sapo.pt>
2018-07-03 10:31:06 +0100

    PropTypes added to navegation link

--- src/Components/Navigation/Link/index.js
+++ src/Components/Navigation/Link/index.js
@@ -1,4 +1,4 @@
-
+impott PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import './index.css';
 
@@ -7,8 +7,13 @@ export default class Link extends Component {
   render() {
     return (
       <div>
-        <a className="Link" href={ this.props.linkurl }>{ this.props.linklabel } </a>
+        <a className="Link" href={this.props.linkurl}>{ this.props.linklabel } </a>
       </div>
-      );
-    }
+    );
   }
+}
+
+Link.propTypes = {
+  linkurl: PropTypes.string.isRequired,
+  linklabel: PropTypes.string.isRequired,
+};
