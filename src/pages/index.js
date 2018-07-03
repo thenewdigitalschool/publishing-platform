856e1e3dc72811182e357c3935f6ffa196f42866
fmmricardo <ricardofilipe5@sapo.pt>
2018-07-03 20:39:54 +0100

    squash commits - ready to merge Final

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
-import React from "react"
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+import Navbar from '../Components/Navbar';
+import Layout from './Layout';
+
+
+export default () => (
+  <Layout>
+    <Navbar />
+  </Layout>
+);
