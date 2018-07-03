b22dfa37ffdedb3f91584cfec4bd7342305bf66f
fmmricardo <ricardofilipe5@sapo.pt>
2018-07-03 10:09:00 +0100

    run yarn install again

--- src/Components/Navigation/Link/index.js
+++ src/Components/Navigation/Link/index.js
@@ -4,11 +4,11 @@ import './index.css';
 
 
 export default class Link extends Component {
-    render() {
-      return (
-        <div>
-        <a className="Link" href={this.props.linkurl}>{this.props.linklabel} </a>
-        </div>
+  render() {
+    return (
+      <div>
+        <a className="Link" href={ this.props.linkurl }>{ this.props.linklabel } </a>
+      </div>
       );
     }
   }
