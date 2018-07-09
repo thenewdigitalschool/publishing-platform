7317ba05667572bb9b0ba24bdb812b0bcc634460
Marissa <marissa.biesecker@gmail.com>
2018-07-09 17:55:01 +0100

    Add comma to link component font file

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -14,9 +14,9 @@ export default class Hero extends Component {
         <div className="Hero-featureContainer">
           <Title>Telmos Story</Title>
           <Quote>
-            "In the school there is not a typical day, like, you always have a
+            In the school there is not a typical day, like, you always have a
             different activies, but the good thing is that you can decide which
-            activities you want to be involved with."
+            activities you want to be involved with.
           </Quote>
         </div>
       </div>
