6fa6a76d8f05e3a4c8969e0cec6e393ae099409f
fmmricardo <ricardofilipe5@sapo.pt>
2018-07-24 12:35:32 +0100

    conflits solved navegation link

--- src/Components/Navigation/Link/index.js
+++ src/Components/Navigation/Link/index.js
@@ -2,36 +2,16 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import './index.css';
 
-<<<<<<< HEAD
-
-// export default class Link extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <a className="Link" href={this.props.linkurl}>{this.props.linklabel} </a>
-//       </div>
-//   }
-// }
-
-=======
->>>>>>> 3e9d892... created the blog branch
 export default class Link extends Component {
   render() {
     return (
       <a className="Link" href={this.props.linkurl}>
         {this.props.linklabel}{' '}
       </a>
-<<<<<<< HEAD
-
-    );
-  }
-}
-=======
     );
   }
 }
 
->>>>>>> 3e9d892... created the blog branch
 Link.propTypes = {
   linkurl: PropTypes.string.isRequired,
   linklabel: PropTypes.string.isRequired,
