c7ed2beea8b2c8d1e5a88a41b819d4c7a71b1e4c
Ricardo Martins <ricardofilipe5@sapo.pt>
2019-06-08 22:40:27 +0100

    install packages to control images from the cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,33 @@ module.exports = {
         name: 'pages',
       },
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/static/images`,
+        name: 'images',
+      },
+    },
     'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 650,
+              linkImagesToOriginal: true,
+            },
+          },
+        ],
+      },
+    },
   ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,33 +1,39 @@
 const path = require('path');
+const { fmImagesToRelative } = require('gatsby-remark-relative-images');
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
 
   const postTemplate = path.resolve('templates/post.js');
 
-  return graphql(`{
-    allMarkdownRemark {
-      edges {
-        node {
-          html
-          id
-          frontmatter {
-            path
-            title
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              path
+              title
+            }
           }
         }
       }
     }
-  }`)
-    .then((res) => {
-      if (res.errors) {
-        return Promise.reject(res.errors);
-      }
-      res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage({
-          path: node.frontmatter.path,
-          component: postTemplate,
-        });
+  `).then((res) => {
+    if (res.errors) {
+      return Promise.reject(res.errors);
+    }
+    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate,
       });
     });
+  });
+};
+
+exports.onCreateNode = ({ node }) => {
+  fmImagesToRelative(node);
 };
