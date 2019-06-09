30372f9e41d3ab2e999659d36c25b9a6abd0a4e6
Ricardo Martins <ricardofilipe5@sapo.pt>
2019-06-09 15:21:56 +0100

    remove path from the cms

--- templates/post.js
+++ templates/post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Layout from '../src/components/Layout';
+import './post.css';
 
 export default function Template({ data }) {
   const { markdownRemark: post } = data;
@@ -8,8 +9,11 @@ export default function Template({ data }) {
     <Layout>
       <div className="Layout-content">
         <div className="Layout-blog">
-          <h1>{post.frontmatter.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+          <h1 className="postTitle">{post.frontmatter.title}</h1>
+          <div
+            className="postImage"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
         </div>
       </div>
     </Layout>
