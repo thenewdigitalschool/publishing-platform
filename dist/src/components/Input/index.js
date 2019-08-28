2087daa79f5fa8dd62451097bf689707d07b7b69
Ricardo Martins <ricardofilipe5@sapo.pt>
2019-08-28 13:37:24 +0100

    add .bitdev

---
+++ dist/src/components/Input/index.js
@@ -0,0 +1,96 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _react = _interopRequireWildcard(require("react"));
+
+require("./index.css");
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+var Input =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(Input, _Component);
+
+  function Input() {
+    _classCallCheck(this, Input);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Input).apply(this, arguments));
+  }
+
+  _createClass(Input, [{
+    key: "renderWithLabel",
+    value: function renderWithLabel() {
+      return _react["default"].createElement("div", {
+        className: "Input"
+      }, _react["default"].createElement("label", {
+        htmlFor: this.props.id,
+        className: "Input-label"
+      }, this.props.label), _react["default"].createElement("input", {
+        id: this.props.id,
+        className: "Input-type",
+        type: this.props.type,
+        placeholder: this.props.placeholder
+      }));
+    }
+  }, {
+    key: "renderWithoutLabel",
+    value: function renderWithoutLabel() {
+      return _react["default"].createElement("div", {
+        className: "Input"
+      }, _react["default"].createElement("input", {
+        id: this.props.id,
+        className: "Input-type",
+        type: this.props.type,
+        placeholder: this.props.placeholder
+      }));
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      if (this.props.label) {
+        return this.renderWithLabel();
+      }
+
+      return this.renderWithoutLabel();
+    }
+  }]);
+
+  return Input;
+}(_react.Component);
+
+exports["default"] = Input;
+Input.propTypes = {
+  id: _propTypes["default"].string.isRequired,
+  label: _propTypes["default"].string.isRequired,
+  placeholder: _propTypes["default"].string.isRequired,
+  type: _propTypes["default"].oneOf(['text', 'email', 'textarea']).isRequired
+};
+
+//# sourceMappingURL=index.js.map
