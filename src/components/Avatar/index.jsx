292d0913576ec1f76ed7141ded51c89216e082bb
Ricardo Martins <ricardofilipe5@sapo.pt>
2019-12-21 23:04:21 +0000

    refactor Avatar

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,18 +1,16 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+
 import './index.css';
 
-export default class Avatar extends Component {
-  render() {
-    return (
-      <div className="Avatar">
-        <img className="Avatar-image" src={this.props.src} alt={this.props.alt} />
-      </div>
-    );
-  }
-}
+const Avatar = ({ src, alt }) => (
+  <div className="Avatar">
+    <img className="Avatar-image" src={src} alt={alt} />
+  </div>
+);
 
 Avatar.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
 };
+export default Avatar;
